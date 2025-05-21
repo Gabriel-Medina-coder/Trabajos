28cd2da601611f7c2e96e2e13eace7fc3bce4905
Gabriel-Medina-coder <20243ds040@utez.edu.mx>
2025-05-21 08:18:40 -0600

    Se creo la carpeta de trabajo

---
+++ Promedio/src/Main.java
@@ -0,0 +1,16 @@
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+        String  calificacion1;
+        String calificacion2;
+        String  calificacion3;
+        float promedio;
+
+            calificacion1 = JOptionPane.showInputDialog("Ingrese la calificación");
+            calificacion2
+
+
+
+    }
+}
